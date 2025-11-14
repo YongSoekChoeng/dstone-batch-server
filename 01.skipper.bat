c4a200190ea9f0980f459a9fcde0b8bc1bea79f6
YongSoekChoeng <jysn007@gmail.com>
2025-11-14 12:39:12 +0900

    .

--- 01.skipper.bat
+++ 01.skipper.bat
@@ -1,1 +1,1 @@
-java -jar ./skipper/spring-cloud-skipper-server-2.11.5.jar --./skipper/spring.config.location=./skipper/skipper-server.yml
+java -jar ./skipper/spring-cloud-skipper-server-2.11.5.jar
