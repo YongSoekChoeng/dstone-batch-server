a300dff871008de025758d3f157de14a0daf3421
YongSoekChoeng <jysn007@gmail.com>
2025-12-17 22:26:24 +0900

    .

--- bin/00.deploy.bat
+++ bin/00.deploy.bat
@@ -1,13 +1,16 @@
-cd ../modules/skipper/spring-cloud-skipper-server-2.11.5
-zip -r -0 ../../../target/spring-cloud-skipper-server-2.11.5.jar META-INF/MANIFEST.MF .
+mkdir ..\target
+mkdir ..\docs\docker\dstone-batch-server\target
 
-cd ../../../
+cd ..\modules\skipper\spring-cloud-skipper-server-2.11.5
+zip -r -0 ..\..\..\target\spring-cloud-skipper-server-2.11.5.jar META-INF\MANIFEST.MF .
+
+cd ..\..\..\
 copy .\target\spring-cloud-skipper-server-2.11.5.jar .\docs\docker\dstone-batch-server\target
 
-cd ./modules/dataflow/spring-cloud-dataflow-server-2.11.5
-zip -r -0 ../../../target/spring-cloud-dataflow-server-2.11.5.jar META-INF/MANIFEST.MF .
+cd .\modules\dataflow\spring-cloud-dataflow-server-2.11.5
+zip -r -0 ..\..\..\target\spring-cloud-dataflow-server-2.11.5.jar META-INF\MANIFEST.MF .
 
-cd ../../../
+cd ..\..\..\
 copy .\target\spring-cloud-dataflow-server-2.11.5.jar .\docs\docker\dstone-batch-server\target
 
 cd bin
