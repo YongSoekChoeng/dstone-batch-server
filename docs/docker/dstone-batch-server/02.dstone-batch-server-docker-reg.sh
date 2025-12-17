aef58b1f3716e401a79e2d200dd008bbde5c1ff3
YongSoekChoeng <jysn007@gmail.com>
2025-12-17 14:14:05 +0900

    .

---
+++ docs/docker/dstone-batch-server/02.dstone-batch-server-docker-reg.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+# Docker Hub에 로그인
+docker login -u jysn007 -p 'db2admin!@'
+# 컨테이너 → 이미지 커밋(버전태깅포함)
+docker commit dstone-batch dstone-batch:v1.0
+# 최종버전태그 추가
+docker tag dstone-batch:v1.0 jysn007/dstone-batch:latest
+# 두개태그(특정했던버전 + 최종버전) 푸시
+docker push jysn007/dstone-batch:v1.0
+docker push jysn007/dstone-batch:latest
+
