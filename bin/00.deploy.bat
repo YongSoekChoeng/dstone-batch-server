62a353ad7608e015943023706f180e5058c6aa8a
YongSoekChoeng <jysn007@gmail.com>
2025-12-17 14:52:42 +0900

    .

--- bin/00.deploy.bat
+++ bin/00.deploy.bat
@@ -1,8 +1,8 @@
-cd ../skipper/spring-cloud-skipper-server-2.11.5
-zip -r -0 ../../target/spring-cloud-skipper-server-2.11.5.jar META-INF/MANIFEST.MF .
+cd ../modules/skipper/spring-cloud-skipper-server-2.11.5
+zip -r -0 ../../../target/spring-cloud-skipper-server-2.11.5.jar META-INF/MANIFEST.MF .
 
-cd ../../
+cd ../../../
 
-cd ./dataflow/spring-cloud-dataflow-server-2.11.5
-zip -r -0 ../../target/spring-cloud-dataflow-server-2.11.5.jar META-INF/MANIFEST.MF .
+cd ./modules/dataflow/spring-cloud-dataflow-server-2.11.5
+zip -r -0 ../../../target/spring-cloud-dataflow-server-2.11.5.jar META-INF/MANIFEST.MF .
 
