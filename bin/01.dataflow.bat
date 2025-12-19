a0dc821c117e8cf43ee1630beb48d8ddb5edf7df
YongSoekChoeng <jysn007@gmail.com>
2025-12-20 06:46:02 +0900

    .

--- bin/01.dataflow.bat
+++ bin/01.dataflow.bat
@@ -1,5 +1,12 @@
 echo "Admin URL : http://localhost:9393/dashboard"
 
+:: env.properties 파일을 한 줄씩 읽어서 환경변수로 세팅
+for /f "usebackq eol=# tokens=1,2 delims==" %%a in ("../conf/env.properties") do (
+    set "%%a=%%b"
+    echo Setting %%a=%%b
+)
+echo 환경변수 세팅 완료!
+
 REM Stream 을 사용할 경우
 REM java -jar ../target/spring-cloud-dataflow-server-2.11.5.jar --spring.config.location=../conf/dataflow-server.yml --spring.cloud.skipper.client.serverUri=http://localhost:7577/api
 
--- bin/01.dataflow.sh
+++ bin/01.dataflow.sh
@@ -2,6 +2,17 @@
 
 echo "Admin URL : http://localhost:9393/dashboard"
 
+# env.properties 파일을 한 줄씩 읽어서 환경변수로 세팅
+while IFS='=' read -r key value; do
+  # 주석(#)으로 시작하는 줄이거나 빈 줄이면 건너뜀
+  [[ "$key" =~ ^#.*$ ]] || [[ -z "$key" ]] && continue  
+  # 환경변수 등록 (export)
+  export "$key=$value"
+  echo "Setting $key=$value"
+done < ../conf/env-dev.properties
+
+echo "환경변수 세팅 완료!"
+
 # Stream 을 사용할 경우
 #java -jar ../target/spring-cloud-dataflow-server-2.11.5.jar --spring.config.location=../conf/dataflow-server.yml --spring.cloud.skipper.client.serverUri=http://localhost:7577/api
 
